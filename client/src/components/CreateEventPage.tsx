4fd751057ec78687209e682587813e211c7419cb
naafiyan <naafiyan@gmail.com>
2021-07-06 22:02:44 +0600

    updated todos on frontend

--- client/src/components/CreateEventPage.tsx
+++ client/src/components/CreateEventPage.tsx
@@ -78,6 +78,8 @@ function FormElement(props: {
     // TODO: handle form sanitization on front end
     // TODO: currently user type is any because I couldn't access user._id. Figure out how to properly export mongoose schemas
     // TODO: find better way to handle duration than in just minutes (kinda confusing to count it - not the most user friendly experience)
+    // TODO: find a way to get location of the rock climbing gym and send that info to the backend
+    
     const handleSubmit = async (form: React.SyntheticEvent) => {
         form.preventDefault();
         // using async await and js fetch api to make post request to backend
