dc2d967813dfa08cd3968b17ca1fd3b98009c1fa
Nicholas Bottone <nick@bottone.io>
2021-08-15 19:47:15 -0400

    Closes #13 - Add GET request for event, cleanup backend errors and validators

--- server/src/routes/calendar.ts
+++ server/src/routes/calendar.ts
@@ -1,22 +1,38 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 import { Error } from "mongoose";
 import { authCheck } from "../middleware/auth";
 import { body } from "express-validator";
 import Event from "../models/Event";
+import { EventType } from "src/types";
+
 const eventRouter = Router();
 
 // GET request that retrieves events, populates with User Schema, and sorts start time of event
-eventRouter.get(
-    "/events",
-    (_req: Request, res: Response, _next: NextFunction) => {
-        Event.find((err: Error, events) => {
-            if (err) console.log(err); // TODO: figure out proper error handling
-            return res.json({ events });
-        })
-            .populate("hostUser")
-            .sort([["startTime", -1]]);
-    }
-);
+eventRouter.get("/events", (_req: Request, res: Response) => {
+    Event.find((err: Error, events: EventType[]) => {
+        if (err) {
+            console.error(err); // TODO: figure out proper error handling
+            res.status(500).send(err);
+            return;
+        }
+        res.json({ events });
+    })
+        .populate("hostUser")
+        .sort([["startTime", -1]]);
+});
+
+// GET request that retrieves a specific event
+eventRouter.get("/event/:eventId", (req: Request, res: Response) => {
+    console.log(req);
+    Event.findById(req.params.eventId, (err: Error, event: EventType) => {
+        if (err) {
+            console.error(err); // TODO: figure out proper error handling
+            res.status(500).send(err);
+            return;
+        }
+        res.json({ event });
+    });
+});
 
 // POST request that allows user to input event and handles possible error for ill formatted event
 eventRouter.post(
@@ -29,11 +45,10 @@ eventRouter.post(
     body("startTime").trim().escape(),
     body("durationAsNumber").trim().escape(),
     body("transportInfo").trim().escape(),
-    body("maxCapacityAsNumber").trim().escape().isNumeric(),
-    (req: Request, res: Response, _next: NextFunction) => {
+    body("maxCapacityAsNumber").trim().escape().isInt({ min: 1, max: 100 }),
+    (req: Request, res: Response) => {
         // Sanitize fields before creating new Event
         //TODO: convert the date to JS date (could use native JS API or Moment)
-        console.log(req.body);
         const event = new Event({
             eventTitle: req.body.eventTitle,
             description: req.body.description,
@@ -48,7 +63,11 @@ eventRouter.post(
         });
 
         event.save((err: Error) => {
-            if (err) console.log(err); // TODO: proper error handling
+            if (err) {
+                console.error(err); // TODO: figure out proper error handling
+                res.status(500).send(err);
+                return;
+            }
             res.status(200).json({
                 event,
                 message: "Event created successfully",
@@ -75,56 +94,52 @@ Example JSON for updating existing event:
 eventRouter.put(
     "/events/:eventId/register", // :eventId is a placeholder and this value can be accessed by req.params.eventId (this is the url path from the frontend)
     authCheck,
-    async (req: Request, res: Response, _next: NextFunction) => {
+    async (req: Request, res: Response) => {
         // checking if received user object from frontend (not receiving means something went wrong)
         if (!req.body.user) {
             res.status(400).json({
-                error: "User information not passed with request",
+                message: "User information not passed with request",
             });
         }
-        // query the db to find the registeredUsers field of the current event
+
+        // check if the user is already registered for the event
         const queryRegisteredUsers = await Event.findById(
             { _id: req.params.eventId },
             "registeredUsers"
         );
-        // if the registeredUsers array does not already include the user that wants to register
         if (queryRegisteredUsers.registeredUsers.includes(req.body.user._id)) {
             res.status(400).json({
-                error: "User is already registered for this event",
+                message: "User is already registered for this event",
             });
         }
 
+        // check if the event is full
         const queryMaxCapacity = await Event.findById(
             { _id: req.params.eventId },
             "maxCapacity"
         );
-        // TODO: check that this max capacity test works
-        // checking if event is past max capacity
         if (
-            queryMaxCapacity.maxCapacity ===
+            queryMaxCapacity.maxCapacity <=
             queryRegisteredUsers.registeredUsers.length
         ) {
             res.status(400).json({
-                error: "Max Capacity Reached",
+                message: "Max event capacity reached",
             });
         }
         // TODO: clean up the if else statements (could probably be more concise and figure out how to handle any errors)
-        // TODO: have a check for number of registered users so that we don't exceed capacity!!
 
-        // user does not exist in registeredUsers so we can proceed
+        // update the registeredUsers field of the current event in the db
         Event.findByIdAndUpdate(
             { _id: req.params.eventId }, // getting event by id
             { $push: { registeredUsers: req.body.user } }, // pushing the user into registeredUsers array
-            { new: true }, // findByIdAndUpdate returns the mongoDB object AFTER the update has been applied
-            (err, result) => {
+            (err) => {
                 if (err) {
-                    res.status(400).json({
-                        error: err,
-                    });
+                    console.error(err); // TODO: figure out proper error handling
+                    res.status(500).send(err);
+                    return;
                 }
-                console.log(result);
                 res.status(200).json({
-                    message: "Successfully added user",
+                    message: "Successfully registered user for event",
                 });
             }
         );
@@ -136,7 +151,7 @@ eventRouter.put(
  * assume that the req.body looks the exact same as in the POST request for creating new events
  * TODO: update the event with the new changes (could probably just update all the fields with everything you get in req.body). Could just create a new Event like I did in the POST request and pass it in as the second argument
  *      to the findByIdAndUpdate method
- * TODO: make sure the person trying to make the put request is infact the hostUser of the event (look at how I used query in the above PUT request to access the registeredUsers field,
+ * TODO: make sure the person trying to make the put request is in fact the hostUser of the event (look at how I used query in the above PUT request to access the registeredUsers field,
  *      will probably want to do something similar for the hostUser field and then compare that user that made the request (assume req.body.user) has the same ._id has the hostUser)
  */
 // editing an event by its host user
